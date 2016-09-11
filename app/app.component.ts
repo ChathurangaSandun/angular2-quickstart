15af6ccf1bd7038be361b7874240758a8d22c5da
chathurangasandun <chathurangasandunkumara@gmail.com>
2016-09-11 14:36:19 +0530

    Initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+@Component({ //meta deata how to interact with  class 
+  selector: 'my-app',
+  template: '<h1>My Second Angular 2 App</h1>'
+})
+export class AppComponent {
+  pageTitle : string = "Chathuranga";
+
+}
